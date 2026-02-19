b7b9428d1e157753c368754977d1d31388aef392
Chiranth Siddappa <chiranthsiddappa@gmail.com>
2026-02-19 10:25:37 -0700

    Refactor `observe` module by introducing `dt_to_ts` helper and adding `range_and_rate` function, and add rate example to observe notebook.

--- src/sat_geo_solver/observe.py
+++ src/sat_geo_solver/observe.py
@@ -1,11 +1,27 @@
 from datetime import datetime
-from typing import List
+from typing import List, Tuple
 
 import numpy as np
 from skyfield.api import load as skyf_load
 from skyfield.api import wgs84
+from skyfield.timelib import Timescale
 
 __ts__ = skyf_load.timescale()
+
+
+def dt_to_ts(dt: datetime | List[datetime]) -> Timescale:
+    """
+    Convert datetime or list of datetimes to Skyfield Timescale.
+
+    :param dt: Datetime or list of datetimes
+    :return: Skyfield Timescale object
+    """
+    if isinstance(dt, datetime):
+        return __ts__.from_datetime(dt)
+    elif isinstance(dt, list):
+        return __ts__.from_datetimes(dt)
+    else:
+        raise TypeError(f"Invalid type for 'dt': {type(dt)}")
 
 
 def distance_to(sat, from_pos: list | np.ndarray, at: datetime | List[datetime]) -> float | List[float]:
@@ -19,10 +35,21 @@ def distance_to(sat, from_pos: list | np.ndarray, at: datetime | List[datetime])
     """
     pos = wgs84.latlon(*from_pos)
     loc_diff = sat - pos
-    if isinstance(at, datetime):
-        observe_ts = __ts__.from_datetime(at)
-    elif isinstance(at, list):
-        observe_ts = __ts__.from_datetimes(at)
-    else:
-        raise TypeError(f"Invalid type for 'at': {type(at)}")
+    observe_ts = dt_to_ts(at)
     return loc_diff.at(observe_ts).distance().km * 1000
+
+
+def range_and_rate(sat, from_pos: list | np.ndarray, at: datetime | List[datetime]) -> Tuple[np.ndarray, np.ndarray]:
+    """
+    Calculate the range and rate of a satellite from a given position at a given time.
+
+    :param sat: Satellite to observe
+    :param from_pos: Position to observe from
+    :param at: Time to observe at
+    :return: Tuple of range and rate in meters and meters per second
+    """
+    pos = wgs84.latlon(*from_pos)
+    loc_diff = sat - pos
+    observe_ts = dt_to_ts(at)
+    _, _, range_distance, _, _, range_rate = loc_diff.at(observe_ts).frame_latlon_and_rates(pos)
+    return range_distance.km * 1000, range_rate.km_per_s * 1000
